9e0f508a23009ac2178632d9b70cbf93564f73cf
Adam <Adam@anope.org>
2018-03-17 15:11:10 -0400

    Format rasterizer 2d

--- cache/src/main/java/net/runelite/cache/item/Rasterizer2D.java
+++ cache/src/main/java/net/runelite/cache/item/Rasterizer2D.java
@@ -2,36 +2,13 @@ package net.runelite.cache.item;
 
 class Rasterizer2D
 {
-
-
 	public int[] graphicsPixels;
-
-
 	public int graphicsPixelsWidth;
-
-
 	public int graphicsPixelsHeight;
-
-
 	public int drawingAreaTop;
-
-
 	public int drawingAreaBottom;
-
-
 	public int draw_region_x;
-
-
 	protected int drawingAreaRight;
-
-//	static
-//	{
-//		drawingAreaTop = 0;
-//		drawingAreaBottom = 0;
-//		draw_region_x = 0;
-//		drawingAreaRight = 0;
-//	}
-
 
 	public void setRasterBuffer(int[] var0, int var1, int var2)
 	{
@@ -40,7 +17,6 @@ class Rasterizer2D
 		graphicsPixelsHeight = var2;
 		setDrawRegion(0, 0, var1, var2);
 	}
-
 
 	public void setDrawRegion(int var0, int var1, int var2, int var3)
 	{
@@ -70,25 +46,6 @@ class Rasterizer2D
 		drawingAreaBottom = var3;
 	}
 
-
-//	public void copyDrawRegion(int[] var0)
-//	{
-//		var0[0] = draw_region_x;
-//		var0[1] = drawingAreaTop;
-//		var0[2] = drawingAreaRight;
-//		var0[3] = drawingAreaBottom;
-//	}
-//
-//
-//	public void setDrawRegion(int[] var0)
-//	{
-//		draw_region_x = var0[0];
-//		drawingAreaTop = var0[1];
-//		drawingAreaRight = var0[2];
-//		drawingAreaBottom = var0[3];
-//	}
-
-
 	public void reset()
 	{
 		int var0 = 0;
@@ -111,58 +68,5 @@ class Rasterizer2D
 		}
 
 	}
-
-
-	public void method5718(int var0, int var1, int var2, int var3)
-	{
-		if (var1 >= drawingAreaTop && var1 < drawingAreaBottom)
-		{
-			if (var0 < draw_region_x)
-			{
-				var2 -= draw_region_x - var0;
-				var0 = draw_region_x;
-			}
-
-			if (var0 + var2 > drawingAreaRight)
-			{
-				var2 = drawingAreaRight - var0;
-			}
-
-			int var4 = var0 + graphicsPixelsWidth * var1;
-
-			for (int var5 = 0; var5 < var2; ++var5)
-			{
-				graphicsPixels[var4 + var5] = var3;
-			}
-
-		}
-	}
-
-
-//	public static void method5707(int var0, int var1, int var2, int var3)
-//	{
-//		if (var0 >= draw_region_x && var0 < drawingAreaRight)
-//		{
-//			if (var1 < drawingAreaTop)
-//			{
-//				var2 -= drawingAreaTop - var1;
-//				var1 = drawingAreaTop;
-//			}
-//
-//			if (var2 + var1 > drawingAreaBottom)
-//			{
-//				var2 = drawingAreaBottom - var1;
-//			}
-//
-//			int var4 = var0 + graphicsPixelsWidth * var1;
-//
-//			for (int var5 = 0; var5 < var2; ++var5)
-//			{
-//				graphicsPixels[var4 + var5 * graphicsPixelsWidth] = var3;
-//			}
-//
-//		}
-//	}
-
 
 }
--- cache/src/main/java/net/runelite/cache/item/Renderable.java
+++ cache/src/main/java/net/runelite/cache/item/Renderable.java
@@ -2,37 +2,10 @@ package net.runelite.cache.item;
 
 abstract class Renderable
 {
-
-
 	public int modelHeight;
 
 	protected Renderable()
 	{
 		this.modelHeight = 1000;
 	}
-
-
-	//   public static IndexedSprite getSprite(IndexDataBase var0, String var1, String var2) {
-//      int var3 = var0.getFile(var1);
-//      int var4 = var0.getChild(var3, var2);
-//      byte[] var7 = var0.getConfigData(var3, var4);
-//      boolean var6;
-//      if(var7 == null) {
-//         var6 = false;
-//      } else {
-//         Widget.decodeSprite(var7);
-//         var6 = true;
-//      }
-//
-//      IndexedSprite var5;
-//      if(!var6) {
-//         var5 = null;
-//      } else {
-//         var5 = Ignore.method5387();
-//      }
-//
-//      return var5;
-//   }
-
-
 }
